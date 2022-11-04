8b0a00fb575652e0e56b891cb003091cb129c009
Louis Zhang <Louisgeemod@gmail.com>
2022-11-04 21:32:33 +0000

    funny and safe-for-work message

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
 var $addEntry = document.querySelector('#add-entry-button');
-$addEntry.addEventListener('click', addEntryHandler);
+$addEntry.addEventListener('click', showModal);
 
 var $overlayModalElement = document.querySelector('.overlay');
 
-function addEntryHandler(event) {
-  console.log($overlayModalElement.className);
+function showModal(event) {
+  $overlayModalElement.className = 'overlay';
 }
+
+function hideModal(event) {
+  $overlayModalElement.className = 'overlay vis-none';
+}
+
+var $modal = document.querySelector('.modal');
+
+$modal.addEventListener('click', function (e) {
+  e.stopPropagation();
+});
+
+$overlayModalElement.addEventListener('click', hideModal);
+
+var $form = document.querySelector('form');
+
+function handleEntrySubmit(event) {
+  event.preventDefault();
+  console.log($form.elements);
+}
+
+$form.addEventListener('submit', handleEntrySubmit);
